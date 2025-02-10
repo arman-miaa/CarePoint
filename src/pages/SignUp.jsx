3969890e65380c5b6152a9dfa584d7bf63e37e3c
Arman <mdarman365506@gmail.com>
2025-02-10 23:29:43 +0600

    some commit deleted from signup page

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,6 +1,6 @@
 import { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
-// import { AuthContext } from "../provider/AuthProvider";
+
 import { toast } from "react-toastify";
 import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Helmet } from "react-helmet";
